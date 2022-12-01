29d373edcb207aa66ecf85731707bec02006fcd9
j3_baiqi <j3_liuliang@163.com>
2022-12-01 22:00:04 +0800

    update

--- ld-base/ld-base-common/src/main/java/cn/j3code/common/config/DruidConfig.java
+++ ld-base/ld-base-common/src/main/java/cn/j3code/common/config/DruidConfig.java
@@ -27,43 +27,46 @@ public class DruidConfig {
 
     @ConfigurationProperties(prefix = "spring.datasource.druid")
     @Bean
-    public DataSource druidDataSource(){
+    public DataSource druidDataSource() {
         return new DruidDataSource();
     }
 
     /**
      * druid 数据源状态监控
      * http://localhost:7210/ld-lucky/ld-druid/login.html
+     *
      * @return
      */
     @Bean
-    public ServletRegistrationBean statViewServlet(){
+    public ServletRegistrationBean statViewServlet() {
         //创建servlet注册实体
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/ld-druid/*");
+        ServletRegistrationBean servletRegistrationBean =
+                new ServletRegistrationBean(new StatViewServlet(), "/ld-druid/*");
         //设置ip白名单(不配置则允许所有)
         // servletRegistrationBean.addInitParameter("allow","127.0.0.1");
         //设置ip黑名单，如果allow与deny共同存在时,deny优先于allow
         // servletRegistrationBean.addInitParameter("deny","192.168.0.19");
         //设置控制台管理用户
-        servletRegistrationBean.addInitParameter("loginUsername","druid");
-        servletRegistrationBean.addInitParameter("loginPassword",druidPassword);
+        servletRegistrationBean.addInitParameter("loginUsername", "druid");
+        servletRegistrationBean.addInitParameter("loginPassword", druidPassword);
         //是否可以重置数据
-        servletRegistrationBean.addInitParameter("resetEnable","false");
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
         return servletRegistrationBean;
     }
 
     /**
      * druid 过滤器
+     *
      * @return
      */
     @Bean
-    public FilterRegistrationBean statFilter(){
+    public FilterRegistrationBean statFilter() {
         //创建过滤器
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
         //设置过滤器过滤路径
         filterRegistrationBean.addUrlPatterns("/*");
         //忽略过滤的形式
-        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         return filterRegistrationBean;
     }
 }
