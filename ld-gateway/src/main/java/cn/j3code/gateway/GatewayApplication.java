fa9c71582424a38ef3cf38e0865996a640257a8e
liuliang <liuliang2@in-smart.cn>
2022-12-01 18:12:19 +0800

    druid配置及监控平台配置

--- ld-gateway/src/main/java/cn/j3code/gateway/GatewayApplication.java
+++ ld-gateway/src/main/java/cn/j3code/gateway/GatewayApplication.java
@@ -12,7 +12,6 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-
     @Bean("ipKeyResolver")
     public IpKeyResolver ipKeyResolver(){
         return new IpKeyResolver();
